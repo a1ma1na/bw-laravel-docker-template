6043f4eb24b9e918b5363a484c681b0272caf164
伊藤美咲 <napori9250@gmail.com>
2025-06-02 17:48:03 +0900

    ToDo新規作成機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -15,4 +15,11 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
+
+    public function create()
+    {
+        return view('todo.create');
+    }
+
 }
+
