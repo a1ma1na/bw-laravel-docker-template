89775d3e064aedff9229b33a83dec832f6530c57
伊藤美咲 <napori9250@gmail.com>
2025-06-09 17:49:47 +0900

    ToDo論理削除機能の実装

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,15 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'todos';
 
     protected $fillable = [
-        'content',
-    ];    
-
+        'content'
+    ];
 }
 
 //Modelを介することでSQL文を組み立てることなくtodosテーブルを操作することができる
