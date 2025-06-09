1c30dc8ced3578f3220ac164d8bd7ebb88b2ea2c
伊藤美咲 <napori9250@gmail.com>
2025-06-09 11:47:50 +0900

    DBのデータを更新

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -58,6 +58,16 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        $todo = $this->todo->findOrFail($id);
+        $todo->fill($inputs);
+        $todo->save();
+        return redirect()->route('todo.show', $todo->id);
+        //dd($inputs);
+    }
+
 
 }
 
--- src/resources/views/todo/edit.blade.php
+++ src/resources/views/todo/edit.blade.php
@@ -5,22 +5,20 @@
     <div class="card">
       <div class="card-header">ToDo編集</div>
       <div class="card-body">
-        <form method="POST" action="">
-          @csrf
-          <div class="form-group row">
-            <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
-            <div class="col-md-6">
-              <input type="text" class="form-control" name="content" value="{{ $todo->content }}">
-            </div>
-          </div>
-          <div class="form-group row mb-0">
-            <div class="col-md-6 offset-md-4">
-              <button type="submit" class="btn btn-primary">更新</button>
-            </div>
-          </div>
-        </form>
-      </div>
+       <form method="POST" action="{{ route('todo.update', $todo->id) }}"> <!-- action属性を追加 -->
+        @csrf
+        @method('PUT')
+        <input type="hidden" name="_method" value="PUT">
+        <div class="form-group row">
+         <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
+        <div class="col-md-6">
+         <input type="text" class="form-control" name="content" value="{{ $todo->content }}">
+        </div>
     </div>
-  </div>
-</div>
+    <div class="form-group row mb-0">
+        <div class="col-md-6 offset-md-4">
+            <button type="submit" class="btn btn-primary">更新</button>
+        </div>
+    </div>
+</form>
 @endsection
